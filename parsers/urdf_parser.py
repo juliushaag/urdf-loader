cd79f83c0cc943733644fa0aaaef5935b8d16178
Julius Haag <haag.julius@outlook.de>
2024-01-27 13:10:56 +0100

    Finished the URDF parser

--- parsers/urdf_parser.py
+++ parsers/urdf_parser.py
@@ -1,57 +1,278 @@
 from dataclasses import dataclass
-from typing import Dict, List
+import os
+from typing import Any, Dict, List, Self, Tuple, TypeVar
 import xml.etree.ElementTree as ET
 
+####### Shared properties #######
+
+XMLNode = ET.Element
+
+def notnone(func): return lambda node: None if node is None else func(node)
+
+def _load_attrib(node : XMLNode, attrib : str, default : Any) -> Any:
+  if node is None or attrib not in node.attrib: return default
+  return type(default)(node.attrib[attrib]) # parse to the type of the default (mostly used for float and int values)
+
+
+def _load_attrib_array(node : XMLNode, attrib : str, default : List[Any], seperator=" "):
+  if node is None or attrib not in node.attrib: return default
+  return [type(default[0])(i) for i in node.attrib[attrib].split(seperator)]
+
+
 @dataclass
-class URDFLinkInertial:
-  originPos : List[float]
-  originRot : List[float]
-  mass : int
-  inertia : Dict[str, int]
+class URDFOrigin:
+  position : List[float]
+  rotation : List[float]
+
+  @notnone
+  @staticmethod
+  def parse(node : XMLNode) -> Self:
+    return URDFOrigin( 
+      _load_attrib_array(node, "xyz", [0.0, 0.0, 0.0]),
+      _load_attrib_array(node, "rpy", [0.0, 0.0, 0.0, 1.0]),
+    )
+  
+  def __repr__(self):
+    return f"{self.position}, {self.rotation}"
+
+@dataclass
+class URDFCalibration:
+  rising : float
+  falling : float
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    return URDFCalibration( 
+      _load_attrib(node, "rising", 0.0),
+      _load_attrib(node, "falling", 0.0)
+    )
+
+@dataclass
+class URDFDynamics:
+  damping : float
+  friction : float
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    return URDFCalibration( 
+      _load_attrib(node, "damping", 0.0),
+      _load_attrib(node, "friction", 0.0)
+    )
+
+@dataclass
+class URDFLimit:
+  lower : float
+  upper : float
+  effort : float
+  velocity : float
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    return URDFLimit(
+      _load_attrib(node, "lower", 0.0),
+      _load_attrib(node, "upper", 0.0),
+      _load_attrib(node, "effort", 0.0),
+      _load_attrib(node, "velocity", 0.0), 
+    )
+
+@dataclass
+class URDFMimic:
+  joint : str
+  mutiplier : float
+  offset : float
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    assert "joint" in node.attrib, "<mimic> field does not contain required attribute" 
+    return URDFMimic(
+      node.attrib["joint"], # required
+      _load_attrib(node, "multiplier", 1.0),
+      _load_attrib(node, "offset", 0.0)
+    )
+
+@dataclass
+class URDFSafetyController:
+  soft_lower_limit : float
+  soft_upper_limit : float
+  k_position : float 
+  k_velocity : float
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    assert "k_velocity" in node.attrib, "<k_velocity> is a required field in the field <safety_controller>"
+    return URDFSafetyController(
+      _load_attrib(node, "soft_lower_limit", 0.0),
+      _load_attrib(node, "soft_upper_limit", 0.0),
+      _load_attrib(node, "k_position", 0.0),
+      _load_attrib(node, "k_velocity", 0.0)
+    )
+
+@dataclass
+class URDFInertia:
+  origin : URDFOrigin
+  mass : float
+  inertia : Dict[str, float]
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    return URDFInertia(
+      URDFOrigin.parse(node.find("origin")),
+      _load_attrib(node, "mass", 0.0),
+      { name : float(value) for name, value in inertia.attrib.items() } if (inertia := node.find("inertia") is not None) else None  # yeah dont care 
+    )
+
 
 @dataclass
 class URDFGeometry:
   type : str
-  size : List[float]
-  fileName : str
+  size : List[float]    # box
+  length : float        # cylinder
+  radius : float        # cylinder and sphere
+  fileName : str        # mesh
+  scale : List[float]
 
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    child = node[0]
+    viztype, = child.tag,
+    assert child is not None, f"<geometry> field has to have a subfield in node with children"
+
+    # very hacky 
+    return URDFGeometry(
+      viztype,
+      _load_attrib_array(child, "size", [1.0, 1.0, 1.0]) if viztype == "box" else None,
+      _load_attrib(child, "length", 1.0) if viztype == "cylinder" else None,
+      _load_attrib(child, "radius", 1.0) if viztype in { "sphere", "cylinder" } else None,
+      _load_attrib(child, "filename", "") if viztype == "mesh" else None,
+      _load_attrib_array(child, "scales", [1.0, 1.0, 1.0])  if viztype == "mesh" else None,
+    )
+  
+  def __repr__(self) -> str:
+    match self.type:
+      case "mesh":
+        return f"mesh at {self.fileName} and scale {self.scale}"
+      case _ :
+        return self.type
 
 @dataclass
 class URDFMaterial:
-  fileName : str
   color : List[float]
+  fileName : str
+  
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    return URDFMaterial(
+      _load_attrib_array(node, "color", [0.0, 0.0, 0.0, 1.0]),
+      _load_attrib(node, "filename", "")
+    )
 
 
 @dataclass
-class URDFLinkVisual:
-  originPos : List[float]
-  originRot : List[float]
+class URDFVisual:
+  name : str
+  origin : URDFOrigin
   geometry : URDFGeometry
   material : URDFMaterial
 
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    assert node.find("geometry") is not None, "<geometry> is a required field in the field <visual>"
+    return URDFVisual(
+      _load_attrib(node, "name", ""),
+      URDFOrigin.parse(node.find("origin")),
+      URDFGeometry.parse(node.find("geometry")),
+      URDFMaterial.parse(node.find("material"))
+    ) 
+  
+  def __repr__(self):
+    return f"{self.geometry}"
+    
+
+
+
 @dataclass
-class URDFLinkCollision:
+class URDFCollision:
   name : str
-  originPos : List[float]
-  originRot : List[float]
+  origin : URDFOrigin
   geometry : URDFGeometry
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    assert node.find("geometry") is not None, "<geometry> is a required field in the field <collision>"
+    return URDFCollision(
+      _load_attrib(node, "name", ""),
+      URDFOrigin.parse(node.find("origin")),
+      URDFGeometry.parse(node.find("geometry"))
+    )
+
+@dataclass
+class URDFLink:
+  name : str
+  inertia : URDFInertia
+  visual : URDFVisual
+  collision : URDFCollision
+
+  @notnone
+  @staticmethod
+  def parse( node : XMLNode):
+    return URDFLink(
+      _load_attrib(node, "name", ""),
+      URDFInertia.parse(node.find("inertia")),
+      URDFVisual.parse(node.find("visual")),
+      URDFCollision.parse(node.find("collsion"))
+    )
+  
+  
+  def __repr__(self) -> str:
+    return f"<URDFLink {self.name} with visual {self.visual}>"
 
 @dataclass
 class URDFJoint:
   name : str
-  type : str  
-  originPos : List[float]
-  originRot : List[float]
+  type : str
+  origin : URDFOrigin
   parent : str
   child : str
-  calibrationRising : float
-  damping : float
-  friction : float
-  effort : float
-  velocity : float
-  lower : float
-  upper : float
-  # skipping safety controllers
+  axis : List[float]
+  calibration : URDFCalibration
+  dynamics : URDFDynamics
+  limit : URDFLimit
+  mimic : URDFLimit
+  safety_controller : URDFSafetyController
+
+  @notnone
+  @staticmethod
+  def parse(node : XMLNode):
+    parent = node.find("parent")
+    child = node.find("child")
+    axis = node.find("axis")
+
+    return URDFJoint(
+      _load_attrib(node, "name", ""),
+      _load_attrib(node, "type", ""),
+      URDFOrigin.parse(node.find("origin")),
+      _load_attrib(parent, "link", ""),
+      _load_attrib(child, "link", ""),
+      _load_attrib_array(axis, "xyz", [1.0, 0.0, 0.0]),
+      URDFCalibration.parse(node.find("calibration")),
+      URDFDynamics.parse(node.find("dynamics")),
+      URDFLimit.parse(node.find("limit")),
+      URDFMimic.parse(node.find("mimic")),
+      URDFSafetyController.parse(node.find("safety_controller"))
+    )
+  
+  def __repr__(self) -> str:
+    return f"<URDFJoint {self.name} of type {self.type} with axis {self.axis} and origin {self.origin}>"
 
 
 class UrdfParser: # http://wiki.ros.org/urdf/XML/model
@@ -59,72 +280,27 @@ class UrdfParser: # http://wiki.ros.org/urdf/XML/model
   def __init__(self, file_name : str):
     fileData = ""
     with open(file_name, "r") as fp: fileData = fp.read()
-    root = ET.XML(fileData)
+
+    robot = ET.XML(fileData)
     
+    assert robot is not None, f"Failed to parse the specified file {file_name} as xml"
+
     self.fileName = file_name
 
-    self.joints = [self.load_joint(joint) for joint in root.findall("joint")]
+    assert robot is not None, f"The supplied file {file_name} does not contain a <robot> tag"
+
+    self.name = robot.attrib["name"] if "name" in robot.attrib else os.path.basename(file_name).split(".")[0] # if no name specified infere it from the file name
+
+    self.joints = [URDFJoint.parse(joint) for joint in robot.findall("joint")]
     
-    self.links = [self.load_link(link) for link in root.findall("link")]
-  
+    self.links = [URDFLink.parse(link) for link in robot.findall("link")]
 
-  def load_joint(self, element): # http://wiki.ros.org/urdf/XML/joint
-    
-    origin = element.find("origin").attrib
-    parent = element.find("parent").attrib
-    child = element.find("child").attrib
-    cali = element.find("calibration").attrib if element.find("calibration") else None
-    dynamics = element.find("dynamics").attrib if element.find("calibration") else None
-    limit = element.find("limit").attrib if element.find("calibration") else None
+    for joint in self.links: print(joint)
 
+    print(self)
 
-    result = URDFJoint(
-      name = element.attrib["name"],
-      type = element.attrib["type"],
-      originPos = [float(num) for num in origin["xyz"].split(" ")],
-      originRot = [float(num) for num in origin["rpy"].split(" ")],
-      parent = parent["link"],
-      child = child["link"],
-      calibrationRising = float(cali["rising"]) if cali is not None else None,
-      damping = dynamics["damping"] if dynamics is not None else None,
-      friction= dynamics["friction"] if dynamics is not None else None,
-      effort=limit['limit'] if limit is not None else None,
-      velocity=['velocity'] if limit is not None else None,
-      lower=limit['lower'] if limit is not None else None,
-      upper=limit["upper"] if limit is not None else None,
-    )
-
-    return result
-  
-
-  def load_link(self, element): # http://wiki.ros.org/urdf/XML/link
-    inertial = element.find("inertial")
-    visual = element.find('visual')
-    collison = element.find("collision")
-
-
-    vistype = visual[0].tag
-    visualResult = URDFLinkVisual(
-      originPos = [float(num) for num in visual.find("origin").attrib["xyz"].split(" ")],
-      originRot = [float(num) for num in visual.find("origin").attrib["rpy"].split(" ")],
-      type = vistype,
-      size = [float(num) for num in visual[0].attrib["size"].split(" ")] if vistype == "box" else None,
-      radius = visual[0].attrib["radius"] if vistype == "sphere" or vistype == "cylinder" else None,
-      length = visual[0].attrib["length"] if vistype == "cylinder" else None,
-      fileName = visual[0].attrib["filename"] if vistype == "mesh" else None,
-      scale = [float(num) for num in visual[0].attrib["scale"].split(" ")] if vistype == "mesh" else None,
-    )
-
-    collisionResult = URDFLinkCollision(
-      name = collison.attrib["name"],
-      originPos = [float(num) for num in collison.find("origin").attrib["xyz"].split(" ")],
-      originRot = [float(num) for num in collison.find("origin").attrib["rpy"].split(" ")],
-      # TODO load geometry
-    )
-    
-    print(element)
-
-
+  def __repr__(self) -> str:
+    return f"<URDFData {self.name}, with {len(self.joints)} joints and {len(self.links)} links>"
 
 if __name__ == "__main__":
-  UrdfParser("res/models/pybullet/robots/panda_arm_hand.urdf")
+  UrdfParser("res/models/pybullet/robots/camera.urdf")
